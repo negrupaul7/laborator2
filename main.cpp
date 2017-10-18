517d833bd1d687b95a4bac74131e8d03a0258ff8
paul <negrupaul7@gmail.com>
2017-10-18 13:45:03 +0300

    exercitiul1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double mile, km;
+    cout << "Lungimea in mile : ";
+    cin >> mile;
+    km = mile * 1.609344;
+    cout << "Lungimea in km este de : " << km << endl;
+    return 0;
+}

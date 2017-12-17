94c8de28d67b700990e0a256b43b6e04bca5def1
paul <negrupaul7@gmail.com>
2017-12-17 18:07:18 +0200

    exercitiul2

--- main.cpp
+++ main.cpp
@@ -4,15 +4,6 @@ using namespace std;
 
 int main()
 {
-    int h, v;
-    double g;
-    cout << "Inaltimea (cm): ";
-    cin >> h;
-    cout << "Varsta (ani): ";
-    cin >> v;
-    g = 50 + 0.75*(h - 150) + 0.25 * (v - 20);
-    cout << "Greutatea ideala (barbat) : " << g << "kg" << endl;
-    cout << "Greutatea ideala (femeie) : " << 0.9 * g << "kg" << endl;
-    return 0;
+
 
 }
